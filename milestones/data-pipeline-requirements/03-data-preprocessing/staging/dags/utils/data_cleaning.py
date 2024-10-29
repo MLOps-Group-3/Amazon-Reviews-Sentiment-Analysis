55bb9e3c8e023b08c2fcda0fb6832cfdfba0f8b7
Harshit S <harshits1910@gmail.com>
2024-10-29 12:26:59 -0400

    [FEAT] Updating data_cleaning.py and test_data_cleaning.py files

--- milestones/data-pipeline-requirements/03-data-preprocessing/staging/dags/utils/data_cleaning.py
+++ milestones/data-pipeline-requirements/03-data-preprocessing/staging/dags/utils/data_cleaning.py
@@ -27,8 +27,8 @@ def clean_amazon_reviews(spark, df_raw):
     # Read CSV as Spark DataFrame
 
     # Filter for specific columns
-    col_list = ['rating', 'title_x', 'text', 'timestamp', 'helpful_vote', 'verified_purchase', 'categories', 'price', 'parent_asin']
-    df_raw = df_raw.select(col_list)
+    # col_list = ['rating', 'title', 'text', 'timestamp', 'helpful_vote', 'verified_purchase', 'categories', 'price', 'parent_asin']
+    # df_raw = df_raw.select(col_list)
 
     # 1. Drop duplicates
     df_raw = df_raw.dropDuplicates()
@@ -37,25 +37,22 @@ def clean_amazon_reviews(spark, df_raw):
     # - Drop reviews with missing text or rating
     df_raw = df_raw.filter(col("text").isNotNull() & col("rating").isNotNull())    
 
-    # - Replace null lists with empty lists in 'categories'
-    df_raw = df_raw.withColumn("categories", split(regexp_replace(col("categories").cast("string"), "null", "[]"), ", "))
-
     # - Handle missing values by replacing NaN with 'unknown', helpful_vote with 0
-    df_raw = df_raw.fillna({"title_x": "unknown", "helpful_vote": 0})
+    df_raw = df_raw.fillna({"title": "unknown", "helpful_vote": 0})
     df_raw = df_raw.withColumn("price", 
                        when(col("price").isNull() | (col("price").cast("string") == "NULL"), "unknown")
                        .otherwise(col("price").cast("string")))
     
     # 3. Clean and normalize text columns
     # - Remove extra whitespaces in 'title_x' and 'text' columns
-    df_raw = df_raw.withColumn("title_x", regexp_replace(col("title_x"), r"\s+", " "))
+    df_raw = df_raw.withColumn("title", regexp_replace(col("title"), r"\s+", " "))
     df_raw = df_raw.withColumn("text", regexp_replace(col("text"), r"\s+", " "))
 
     return df_raw
 
 if __name__ == "__main__":
     spark = create_spark_session()
-    file_path = 'milestones/data-pipeline-requirements/03-data-preprocessing/staging/data/sampled_2018.csv' 
+    file_path = 'milestones/data-pipeline-requirements/03-data-preprocessing/staging/data/sampled_data_2018_2019.csv' 
     df_raw = spark.read.csv(file_path, header=True, inferSchema=True, quote='"',escape='"')
 
     df_cleaned = clean_amazon_reviews(spark, df_raw)
