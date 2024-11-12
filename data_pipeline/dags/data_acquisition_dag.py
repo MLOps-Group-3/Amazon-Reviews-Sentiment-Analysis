ce1656f43b8f55e7c21a7274f2bc3a5a43b7054c
Niresh S <subramanian.ni@northeastern.edu>
2024-11-12 16:39:01 -0500

    Pushing final files

--- data_pipeline/dags/data_acquisition_dag.py
+++ data_pipeline/dags/data_acquisition_dag.py
@@ -9,15 +9,6 @@ from airflow.models import Variable
 # Set the LOG_DIRECTORY variable
 Variable.set("LOG_DIRECTORY", "/opt/airflow/logs", description="Directory for storing logs")
 
-# default_args = {
-#     'owner': 'airflow',
-#     'depends_on_past': False,
-#     'start_date': datetime(2024, 10, 30),
-#     'email_on_failure': True,
-#     'email_on_retry': True,
-#     'retries': 1,
-#     'retry_delay': timedelta(minutes=5)
-# }
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 10, 22),
@@ -44,23 +35,6 @@ acquire_data_task = PythonOperator(
     python_callable=acquire_data,
     dag=dag,
 )
-
-# send_success_email = EmailOperator(
-#     task_id='send_success_email',
-#     # to='subramanian.ni@northeastern.edu',
-#     subject='Data Acquisition Task Completed Successfully',
-#     html_content='The data acquisition task for Amazon reviews has been completed successfully.',
-#     dag=dag,
-# )
-
-# send_failure_email = EmailOperator(
-#     task_id='send_failure_email',
-#     to='subraamanian.ni@northeastern.edu',
-#     subject='Data Acquisition Task Failed',
-#     html_content='The data acquisition task for Amazon reviews has failed.',
-#     dag=dag,
-#     trigger_rule='one_failed'
-# )
 
 trigger_sampling_dag = TriggerDagRunOperator(
     task_id='trigger_sampling_dag',
