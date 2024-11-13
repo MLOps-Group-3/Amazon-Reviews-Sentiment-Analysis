6f0d666e8208b915fc210c40e52d737c1cc7c2e8
vallimeenaav <vellaiyan.v@northeastern.edu>
2024-11-12 20:20:01 -0500

    Update and restructure dag for rag pipeline

---
+++ model_pipeline/rag/dags/utils/config.py
@@ -0,0 +1,7 @@
+PROCESSED_DATA_PATH = "/opt/airflow/data/labeled_data_test.csv"
+DOCUMENT_STORE_PATH = "/opt/airflow/data/document_store_1.json"
+REFINED_PROCESSED_DATA_PATH = "/opt/airflow/data/refined_processed_documents.json"
+DOCUMENTS_FILE_PATH = "/opt/airflow/data/document_store_1.json"
+PROCESSED_OUTPUT_PATH = "/opt/airflow/data/processed_documents.json"
+REFINED_OUTPUT_PATH = "/opt/airflow/data/refined_processed_documents.json"
+PROMPT_TEMPLATE_PATH = "/opt/airflow/dags/utils/prompt.txt"
