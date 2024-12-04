7319519ad7f9683b2cfef803f3027c99458e7a8f
vallimeenaav <vellaiyan.v@northeastern.edu>
2024-12-04 02:14:22 -0500

    Training sample files update

--- data_pipeline_sampling_test/dags/sampling_train_dag.py
+++ data_pipeline_sampling_test/dags/sampling_train_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.utils.task_group import TaskGroup
 from datetime import datetime, timedelta
 from utils.data_collection.sampling_train import sample_training_data
 from utils.data_collection.data_concat_train import concatenate_and_save_csv_files
@@ -34,81 +35,81 @@ default_args = {
 
 # Define the DAG
 with DAG(
-    dag_id='sampling_train_dag',
+    dag_id='sampling_train_dag_1',
     default_args=default_args,
-    description='DAG to sample training data dynamically',
+    description='DAG to sample training data dynamically and sequentially',
     schedule_interval=None,
     catchup=False,
     max_active_runs=1,
 ) as dag:
 
-    # Task to pull data from GCS to local
-    pull_from_gcs_task = PythonOperator(
-        task_id='pull_from_gcs',
-        python_callable=pull_from_gcs,
-        op_kwargs={
-            'bucket_name': GCS_BUCKET_NAME,
-            'source_blob_prefix': 'data/sampled/training/',
-            'local_directory': SAMPLED_TRAINING_DIRECTORY,
-            'service_account_key': GCS_SERVICE_ACCOUNT_KEY
-        },
-    )
-
-    # Create tasks for each category
-    category_tasks = []
-    for category_name in CATEGORIES:
-        # Get the next training period for this category
-        training_start_date, training_end_date = get_next_training_period(
-            SAMPLED_TRAINING_DIRECTORY, 
-            category_name, 
-            default_start_year=DEFAULT_TRAINING_START_YEAR,
-            default_start_month=DEFAULT_TRAINING_START_MONTH
-        )
-        
-        task = PythonOperator(
-            task_id=f'sample_training_{category_name}',
-            python_callable=sample_training_data,
+    # Task Group 1: Pull from GCS
+    with TaskGroup("pull_from_gcs_group") as pull_from_gcs_group:
+        pull_from_gcs_task = PythonOperator(
+            task_id='pull_from_gcs',
+            python_callable=pull_from_gcs,
             op_kwargs={
-                'category_name': category_name,
-                'start_date': training_start_date,
-                'end_date': training_end_date,
+                'bucket_name': GCS_BUCKET_NAME,
+                'source_blob_prefix': 'data/sampled/training/',
+                'local_directory': SAMPLED_TRAINING_DIRECTORY,
+                'service_account_key': GCS_SERVICE_ACCOUNT_KEY
             },
         )
-        category_tasks.append(task)
 
-    # Dynamically compute start and end dates for concatenation filename
-    concat_start_date, concat_end_date = get_next_training_period(
-        SAMPLED_TRAINING_DIRECTORY,
-        CATEGORIES[0],  # Assuming all categories share the same training period
-        default_start_year=DEFAULT_TRAINING_START_YEAR,
-        default_start_month=DEFAULT_TRAINING_START_MONTH
-    )
+    # Task Group 2: Sampling and Concatenation
+    with TaskGroup("sampling_and_concatenation_group") as sampling_and_concatenation_group:
+        previous_task = None
 
-    # Create a task to concatenate data after all categories are sampled
-    concat_task = PythonOperator(
-        task_id='concatenate_training_data',
-        python_callable=concatenate_and_save_csv_files,
-        op_kwargs={
-            'input_dir': SAMPLED_TRAINING_DIRECTORY,
-            'output_file': f'{SAMPLED_TRAINING_DIRECTORY}/concatenated_training_data_{concat_start_date}_to_{concat_end_date}.csv',
-        },
-    )
+        for category_name in CATEGORIES:
+            # Get the next training period for this category
+            training_start_date, training_end_date = get_next_training_period(
+                SAMPLED_TRAINING_DIRECTORY,
+                category_name,
+                default_start_year=DEFAULT_TRAINING_START_YEAR,
+                default_start_month=DEFAULT_TRAINING_START_MONTH,
+            )
 
-    # Task to push data from local to GCS
-    push_to_gcs_task = PythonOperator(
-        task_id='push_to_gcs',
-        python_callable=push_to_gcs,
-        op_kwargs={
-            'bucket_name': GCS_BUCKET_NAME,
-            'source_directory': SAMPLED_TRAINING_DIRECTORY,
-            'destination_blob_prefix': 'data/sampled/training/',
-            'service_account_key': GCS_SERVICE_ACCOUNT_KEY
-        },
-    )
+            # Sampling task for the current category
+            sampling_task = PythonOperator(
+                task_id=f'sample_training_{category_name}',
+                python_callable=sample_training_data,
+                op_kwargs={
+                    'category_name': category_name,
+                    'start_date': training_start_date,
+                    'end_date': training_end_date,
+                },
+            )
 
-    # Set up task dependencies
-    pull_from_gcs_task >> category_tasks[0]
-    if len(category_tasks) > 1:
-        for i in range(len(category_tasks) - 1):
-            category_tasks[i] >> category_tasks[i + 1]
-    category_tasks[-1] >> concat_task >> push_to_gcs_task
+            # Concatenation task for the current category
+            concatenation_task = PythonOperator(
+                task_id=f'concatenate_training_{category_name}',
+                python_callable=concatenate_and_save_csv_files,
+                op_kwargs={
+                    'input_dir': SAMPLED_TRAINING_DIRECTORY,
+                    'output_file': f'{SAMPLED_TRAINING_DIRECTORY}/concatenated_training_data_{category_name}.csv',
+                },
+            )
+
+            # Set sequential dependencies between sampling and concatenation
+            sampling_task >> concatenation_task
+
+            # Chain tasks sequentially across categories
+            if previous_task:
+                previous_task >> sampling_task
+            previous_task = concatenation_task  # Update the last task to point to the concatenation task
+
+    # Task Group 3: Push to GCS
+    with TaskGroup("push_to_gcs_group") as push_to_gcs_group:
+        push_to_gcs_task = PythonOperator(
+            task_id='push_to_gcs',
+            python_callable=push_to_gcs,
+            op_kwargs={
+                'bucket_name': GCS_BUCKET_NAME,
+                'source_directory': SAMPLED_TRAINING_DIRECTORY,
+                'destination_blob_prefix': 'data/sampled/training/',
+                'service_account_key': GCS_SERVICE_ACCOUNT_KEY
+            },
+        )
+
+    # Define the overall DAG structure
+    pull_from_gcs_group >> sampling_and_concatenation_group >> push_to_gcs_group
