9de32dbd9fc3a13d834de6f98e1bb301af2d586d
Niresh S <subramanian.ni@northeastern.edu>
2024-12-04 13:50:01 -0500

    Updating train sample with push to gcs

--- data_pipeline_sampling_test/dags/sampling_train_dag.py
+++ data_pipeline_sampling_test/dags/sampling_train_dag.py
@@ -5,7 +5,16 @@ from datetime import datetime, timedelta
 from utils.data_collection.sampling_train import sample_training_data
 from utils.data_collection.data_concat_serve import concatenate_and_save_csv_files
 from utils.data_collection.dynamic_month_train import get_next_training_period
+from utils.data_collection.gcs_operations import push_to_gcs
 from utils.config import CATEGORIES, SAMPLED_TRAINING_DIRECTORY
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+GCS_SERVICE_ACCOUNT_KEY = os.getenv("GCS_SERVICE_ACCOUNT_KEY", "/opt/airflow/config/amazonreviewssentimentanalysis-8dfde6e21c1d.json")
+GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME_MODEL", "model-deployment-from-airflow")
 
 # Default arguments for the DAG
 default_args = {
@@ -22,7 +31,7 @@ with DAG(
     dag_id='03_sampling_train_dag',
     default_args=default_args,
     description='DAG to sample training data dynamically',
-    schedule_interval=None,  # Run on the 2nd day of every three months
+    schedule_interval=None,
     catchup=False,
     max_active_runs=1,
 ) as dag:
@@ -54,7 +63,19 @@ with DAG(
         },
     )
 
-    # Trigger data validation DAG after concatenation
+    # Create a task to push the concatenated data to GCS
+    push_to_gcs_task = PythonOperator(
+        task_id='push_to_gcs',
+        python_callable=push_to_gcs,
+        op_kwargs={
+            'bucket_name': GCS_BUCKET_NAME,
+            'source_directory': SAMPLED_TRAINING_DIRECTORY,
+            'destination_blob_prefix': 'data/sampled/training/',
+            'service_account_key': GCS_SERVICE_ACCOUNT_KEY,
+        },
+    )
+
+    # Trigger data validation DAG after the GCS push
     trigger_validation_dag = TriggerDagRunOperator(
         task_id='trigger_validation_dag',
         trigger_dag_id='03_data_validation_dag',
@@ -65,6 +86,6 @@ with DAG(
     if category_tasks:
         for i in range(len(category_tasks) - 1):
             category_tasks[i] >> category_tasks[i + 1]
-        category_tasks[-1] >> concat_task # >> trigger_validation_dag
+        category_tasks[-1] >> concat_task >> push_to_gcs_task >> trigger_validation_dag
     else:
-        concat_task # >> trigger_validation_dag
+        concat_task >> push_to_gcs_task >> trigger_validation_dag
